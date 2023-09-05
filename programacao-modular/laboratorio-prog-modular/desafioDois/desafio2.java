4c7b2d8a20e89567156e0409dddaf3b0e54a525a
Lucas de Carvalho Barbosa <103704337+LucasCBar@users.noreply.github.com>
2023-09-04 23:35:42 -0300

    Update desafio2.java

--- programacao-modular/laboratorio-prog-modular/desafioDois/desafio2.java
+++ programacao-modular/laboratorio-prog-modular/desafioDois/desafio2.java
@@ -2,6 +2,15 @@ package desafioDois;
 import java.util.Scanner;
 public class desafio2 {
 
+
+
+
+
+
+
+
+
+	
 	public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         
